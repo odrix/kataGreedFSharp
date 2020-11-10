1c3052fee30560801d04ca0f7ccc27d7f6c77555
odrix <adribey@free.fr>
2020-11-11 00:08:24 +0100

    renomage

--- Greed.fs
+++ Greed.fs
@@ -1,8 +1,8 @@
 module Greed
 
 let GreedScore (dices) : int = 
-    let results = dices |> Seq.countBy id |> Seq.toList 
-    match results.Item(0) with
+    let diceCountBy = dices |> Seq.countBy id |> Seq.toList 
+    match diceCountBy.Item(0) with
     | (1, 1) -> 100
     | (1, 3) -> 1000
     | (x, 3) -> x * 100
--- GreedTest.fs
+++ GreedTest.fs
@@ -2,31 +2,24 @@
 open Greed
 open NUnit.Framework
 
-[<SetUp>]
-let Setup () =
-    ()
 
-[<Test>]
-let OneOne_Should_Score100 () =    
-    let dices = [1]
-    Assert.AreEqual(100, GreedScore(dices))
-
-[<Test>]
-let OneFive_Should_Score50 () =    
-    let dices = [5]
-    Assert.AreEqual(50, GreedScore(dices))
+[<TestCase(1, 100, TestName="One one score 100")>]
+[<TestCase(5, 50, TestName="One five score 50")>]
+[<TestCase(3, 0)>]
+let JustOneDice_Should_ScoreSome (dice, expectedScore) =    
+    let dices = [dice]
+    Assert.AreEqual(expectedScore, GreedScore(dices))
 
 [<Test>]
 let ThreeOne_Should_Score1000 () =    
     let dices = [1;1;1]
     Assert.AreEqual(1000, GreedScore(dices))
 
-
 [<TestCase(2, 200)>]
 [<TestCase(3, 300)>]
 [<TestCase(4, 400)>]
 [<TestCase(5, 500)>]
 [<TestCase(6, 600)>]
-let ThreeSame_Should_ScoreX100 (dicesValue, expectedScore) =    
-    let dices = [for i in 1..3 -> dicesValue]
+let ThreeSameDice_Should_ScoreDiceX100 (diceValue, expectedScore) =    
+    let dices = [for i in 1..3 -> diceValue]
     Assert.AreEqual(expectedScore, GreedScore(dices))
